da11744e1715505083acccc097285e936c3d36b7
Endy4elect <endy4elect@yahoo.com>
2022-11-14 10:16:09 -0800

    environ.c

--- environ.c
+++ environ.c
@@ -3,9 +3,10 @@
 /**
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * constant function prototype.
  * Return: Always 0
  */
+
 int _myenv(info_t *info)
 {
 	print_list_str(info->env);
@@ -15,7 +16,7 @@ int _myenv(info_t *info)
 /**
  * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
+ * @name: env var name.
  *
  * Return: the value
  */
