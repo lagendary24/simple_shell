1e35d4258ae0f1b97380182d39c095ab9d4e97eb
Endy4elect <endy4elect@yahoo.com>
2022-11-14 10:24:33 -0800

    string1.c

--- string1.c
+++ string1.c
@@ -3,10 +3,11 @@
 /**
  * _strcpy - copies a string
  * @dest: the destination
- * @src: the source
+ * @src: the source.
  *
  * Return: pointer to destination
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
