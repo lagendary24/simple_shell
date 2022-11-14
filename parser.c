f1fb25b953842fa6ca9f30812676352d257adcec
Endy4elect <endy4elect@yahoo.com>
2022-11-14 10:22:36 -0800

    parser.c

--- parser.c
+++ parser.c
@@ -3,10 +3,11 @@
 /**
  * is_cmd - determines if a file is an executable command
  * @info: the info struct
- * @path: path to the file
+ * @path: path to the file.
  *
  * Return: 1 if true, 0 otherwise
  */
+
 int is_cmd(info_t *info, char *path)
 {
 	struct stat st;
