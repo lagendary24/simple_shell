8573702d47d2dc6a3fc51198df5f3aa8532bc0e5
Endy4elect <endy4elect@yahoo.com>
2022-11-14 19:04:51 -0800

    errors.c

--- errors.c
+++ errors.c
@@ -2,10 +2,11 @@
 
 /**
  *_eputs - prints an input string
- * @str: the string to be printed
+ * @str: the string to be printed.
  *
  * Return: Nothing
  */
+
 void _eputs(char *str)
 {
 	int i = 0;
@@ -26,6 +27,7 @@ void _eputs(char *str)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _eputchar(char c)
 {
 	static int i;
@@ -44,11 +46,12 @@ int _eputchar(char c)
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
- * @fd: The filedescriptor to write to
+ * @fd: The filedescriptor to write to.
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putfd(char c, int fd)
 {
 	static int i;
@@ -67,10 +70,11 @@ int _putfd(char c, int fd)
 /**
  *_putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the filedescriptor to write to
+ * @fd: the filedescriptor to write to.
  *
  * Return: the number of chars put
  */
+
 int _putsfd(char *str, int fd)
 {
 	int i = 0;
